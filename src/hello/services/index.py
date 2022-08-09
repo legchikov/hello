23ada550f4e1241b6e037b62399d7d0b926272e0
d.legchikov <Obninsk@123>
2022-08-09 22:43:42 +0300

    add

--- src/hello/services/index.py
+++ src/hello/services/index.py
@@ -3,3 +3,10 @@ def run_index() -> list[int]:
     Run indexing
     """
     return [1, 2, 3]
+
+
+def run_super_index() -> list[int]:
+    """
+    Run indexing
+    """
+    return [1, 2, 3, 4, 5]
